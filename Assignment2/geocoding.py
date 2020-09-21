4b71b218e0807a9ff75f29a2021e960758428223
ERIK <erikliu0801@gmail.com>
2020-09-21 23:12:54 +0800

    + assignment2

---
+++ Assignment2/geocoding.py
@@ -0,0 +1,30 @@
+import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+
+def get_coordinate(browser, addr):
+    browser.get("http://www.map.com.tw/")
+    search = browser.find_element_by_id("searchWord")
+    search.clear()
+    search.send_keys(addr)
+    browser.find_element_by_xpath("/html/body/form/div[10]/div[2]/img[2]").click() 
+    time.sleep(2)
+    iframe = browser.find_elements_by_tag_name("iframe")[1]
+    browser.switch_to.frame(iframe)
+    coor_btn = browser.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]")
+    coor_btn.click()
+    coor = browser.find_element_by_xpath("/html/body/form/div[5]/table/tbody/tr[2]/td")
+    coor = coor.text.strip().split(" ")
+    lat = coor[-1].split("：")[-1]
+    log = coor[0].split("：")[-1]
+    browser.quit()
+    print(addr, lat, log)
+    return (lat, log)
+
+if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    browser = webdriver.Chrome(executable_path='chromedriver',options=options)
+    get_coordinate(browser, "桃園市桃園區正光二街181號")
